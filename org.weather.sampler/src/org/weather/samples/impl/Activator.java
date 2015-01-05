74e6a0097c4fdb8ee8add79742b3ad1024630aae
OUNavCon <isaacdrose@gmail.com>
2015-01-05 17:25:42 -0500

    Added collections to task.

--- org.weather.sampler/src/org/weather/samples/impl/Activator.java
+++ org.weather.sampler/src/org/weather/samples/impl/Activator.java
@@ -5,6 +5,8 @@ import java.util.Properties;
 import org.apache.felix.dm.DependencyActivatorBase;
 import org.apache.felix.dm.DependencyManager;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.service.cm.ManagedService;
 import org.osgi.service.log.LogService;
 import org.quartz.Job;
 import org.weather.TemperatureService;
@@ -23,10 +25,10 @@ public class Activator extends DependencyActivatorBase{
 			throws Exception {
 		
 		Properties props = new Properties();
-		props.put("service.pid", "org.weathersampler");
+		props.put(Constants.SERVICE_PID, "org.weathersampler");
 		
 		dm.add(createComponent()
-				.setInterface(WeatherSamples.class.getName(),props)
+				.setInterface(WeatherSamples.class.getName(),null)
 				.setImplementation(WeatherSamplesImpl.class)
 				.add(createServiceDependency()
 						.setService(TemperatureService.class)
@@ -36,11 +38,18 @@ public class Activator extends DependencyActivatorBase{
 						.setRequired(false)));		
 		
 		dm.add(createComponent()
-				.setInterface(Job.class.getName(), props)
+				.setInterface(Job.class.getName(), null)
 				.setImplementation(WeatherSamplesImpl.class)
 				.add(createServiceDependency()
 						.setService(TemperatureService.class)
 						.setRequired(true))
+				.add(createServiceDependency()
+						.setService(LogService.class)
+						.setRequired(false)));
+		
+		dm.add(createComponent()
+				.setInterface(ManagedService.class.getName(), props)
+				.setImplementation(WeatherSamplesImpl.class)
 				.add(createServiceDependency()
 						.setService(LogService.class)
 						.setRequired(false)));
