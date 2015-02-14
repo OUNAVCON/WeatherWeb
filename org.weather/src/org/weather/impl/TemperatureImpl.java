0127360a52a2eb0d642958ce0274f66d73af5fe2
Isaac Rose <isaacdrose@gmail.com>
2015-02-14 00:18:14 -0500

    Added Pi4j to temperature bundles to test led blink

--- org.weather/src/org/weather/impl/TemperatureImpl.java
+++ org.weather/src/org/weather/impl/TemperatureImpl.java
@@ -4,7 +4,11 @@ import java.util.Random;
 
 import org.weather.TemperatureService;
 
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.RaspiPin;
+
 public class TemperatureImpl implements TemperatureService {
+
 
 	private double result = 90;
 	private Random rand = new Random();
@@ -12,6 +16,20 @@ public class TemperatureImpl implements TemperatureService {
 	
 	@Override
 	public double getTemperature() {
+    System.out.println("<--Pi4J--> GPIO Blink Example ... started.");
+        
+        // create gpio controller
+        final com.pi4j.io.gpio.GpioController gpio = GpioFactory.getInstance();
+        
+        // provision gpio pin #01 as an output pins and blink
+        final com.pi4j.io.gpio.GpioPinDigitalOutput led1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_18);
+
+		if (led1.isHigh()) {
+			led1.setState(false);
+		} else {
+			led1.setState(true);
+		}
+          
 		result += (rand.nextDouble() - 0.5F);
 		return result;
 	}
