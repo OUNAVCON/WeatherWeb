53dd73e511f6d00ef596d362f746c8a2f33f6a8a
Isaac Rose <isaacdrose@gmail.com>
2015-03-31 23:11:39 -0400

    Removed temporal files.

--- webui/app/bootstrap.js
+++
@@ -1,9 +0,0 @@
-define(['require', 'angular', 'amdatu.bootstrap.app'], function(require, ng) {
-    'use strict';
-
-    require(['domReady!'], function(document) {
-        /* everything is loaded...go! */
-
-        return ng.bootstrap(document, ['amdatu.bootstrap.app']);
-    });
-});
